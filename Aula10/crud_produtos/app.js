476132e1d603aca2955c3d33d7e56ddbd8024a16
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-10-18 23:25:34 -0300

    Aula 10 crud Produtos

--- Aula10/crud_produtos/app.js
+++ Aula10/crud_produtos/app.js
@@ -10,25 +10,77 @@ const conexao = {
 
 async function listar() {
     const cliente = new Client(conexao)
-
-    await cliente.connect()
-
-    const res = await cliente.query('SELECT * FROM produtos')
-    console.log(res.rows)
-    await cliente.end()
-
+    await cliente.connect();
+    const res = await cliente.query('SELECT * FROM produtos');
+    await cliente.end();
+    return res.rows;
 }
-
-async function inserir(produtos) {
+async function inserir(produto) {
     const cliente = new Client(conexao)
 
-    await cliente.connect()
+    await cliente.connect();
 
-    const res = await cliente.query('INSERT INTO produtos(nome, preco) VALUES ($1, $2)'),
-    [produto.nome, produto.preco]
-    console.log(res.rows)
-    await cliente.end()
-
+    const res = await cliente.query('INSERT INTO produtos(nome,preco) VALUES ($1,$2) RETURNING *', 
+        [produto.nome, produto.preco]);
+    await cliente.end();
+    return res.rows[0]
 }
 
-listar();
+async function buscarPorId(id) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('SELECT * FROM produtos WHERE id=$1',[id]);
+    await cliente.end();
+    return res.rows[0];
+}
+
+async function buscarPorNome(nome) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('SELECT * FROM produtos WHERE nome=$1',[nome]);
+    await cliente.end();
+    return res.rows;
+}
+
+async function atualizar(id, produto) {
+    const cliente = new Client(conexao)
+
+    await cliente.connect();
+
+    const res = await cliente.query('UPDATE produtos SET nome=$1, preco=$2 WHERE id=$3 RETURNING *', 
+        [produto.nome, produto.preco, id]);
+    await cliente.end();
+    return res.rows[0]
+}
+
+async function deletar(id) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('DELETE FROM produtos WHERE id=$1 RETURNING *',[id]);
+    await cliente.end();
+    return res.rows[0];
+}
+
+async function main() {
+    const produtoInserido = await inserir({nome: "produto3", preco: 20})
+    console.log("Produto Inserido", produtoInserido);
+    
+    const listaProdutos = await listar();
+    console.log("Lista de Produtos",listaProdutos);
+
+    const produto3 = await buscarPorId(3);
+    console.log("Produto 3", produto3);
+
+    const produtoProd3 = await buscarPorNome('produto3');
+    console.log("Produto nome=3", produtoProd3);
+
+    const produtoAtualizado = await atualizar(4, { nome: 'produto4', preco: 25});
+    console.log("Produto atualizado", produtoAtualizado);
+
+    const produtoDeletado = await deletar(6);
+    console.log("Produto deletado", produtoDeletado);
+
+ 
+}
+
+main();
